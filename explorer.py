d0cfa811052141043ed4765c38fc019c61ed7d5d
gabriel <gabriel.bizzotto@gmail.com>
2021-09-05 14:43:06 -0300

    Buffer get_current_height().

--- explorer.py
+++ explorer.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 import json
+import time
 
 import transactions
 
@@ -67,9 +68,18 @@ def get_output_scriptpubkey(txid, vout, testnet):
     return json.loads(page.text)["vout"][vout]["scriptpubkey"]
 
 def get_current_height(testnet):
+    epoch = time.time()
+    print(epoch)
+    if get_current_height.epoch is not None and epoch < get_current_height.epoch+60:
+        return get_current_height.height
+
     print("explorer get_current_height")
+    get_current_height.epoch = epoch
 
     # blockstream
     network = "testnet/" if testnet else ""
     page = requests.get("https://blockstream.info/"+network+"/api/blocks/tip/height")
-    return int(page.text)
+    get_current_height.height = int(page.text)
+    return get_current_height.height
+get_current_height.height = None
+get_current_height.epoch  = None
